46148910ffedd66353771ff5ba713245ce751dd5
HieuTech <rosasuong.tech@gmail.com>
2024-01-30 09:32:18 +0700

    test

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace CS001_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Xin chào C# NET CORE!");
+        }
+    }
+}
